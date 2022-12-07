014fdcbe4d155a8fa0c01dc1736b63f2559c19e5
Christian Graham <christian.graham18@gmail.com>
2022-12-06 19:53:08 -0600

    moved database to MySQL

--- ToDoApp/settings.py
+++ ToDoApp/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'ToDoApp.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'task_tracker',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+        'USER': 'root',
+        'PASSWORD': 'Persia1897!',
     }
 }
 
